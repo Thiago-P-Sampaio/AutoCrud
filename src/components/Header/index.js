b89ef28e8b3b177243fcb4dd03ea4465bae49460
Felipe <fearaujo342@gmail.com>
2025-04-22 16:45:55 -0300

    Estruturando o Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <h1 className='title'>AutoCrud</h1>
+    </header>
+  );
+};
+
