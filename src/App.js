e4cfe70903caaba0309553fe2eadfc43ba6ccf5b
Felipe <fearaujo342@gmail.com>
2025-04-28 10:39:13 -0300

    Estruturando a página e implementando responsividade

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import Main from "./Pages/Main"
+import Main from "./Pages/Main";
 import { Flip, ToastContainer } from "react-toastify";
+import Header from "./components/Header";
+import "./App.css";
 
 
 export default function App(){
@@ -14,6 +16,7 @@ export default function App(){
     transition={Flip}
     limit={ 3 }
     />
+    <Header />
     <Main />
     </>
   )
